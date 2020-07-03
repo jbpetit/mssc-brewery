4412445ecf7df98017647d5a4e530d731e5963be
Jeff Petit <jeffreybpetit@outlook.com>
2020-07-03 11:18:46 -0500

    versioning test

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
 
@@ -14,5 +16,19 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder().id(UUID.randomUUID()).build();
+    }
+
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        // TODO - Add a real impl to update beer.
+    }
+
+    @Override
+    public void deleteByUd(UUID beerId) {
+        log.debug("Deleting a beer ...");
     }
 }
